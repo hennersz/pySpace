5b321b1df806d13c9d5b0f0c1348a28984eb029e
Henry Mortimer <henry@morti.plus.com>
2017-02-09 18:25:02 +0000

    added plot

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from satelliteSimulator.keplerianPropogation import propogateOrbit
-from satelliteSimulator.data import *
-from satelliteSimulator.utils import readData
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
-propogateOrbit(jason['R'], jason['V'], 10, 8600)
-data = readData('./data/kepProp.csv')
-print(data[0])
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x =[1,2,3,4,5,6,7,8,9,10]
+y =[5,6,2,3,13,4,1,2,4,8]
+z =[2,3,3,3,5,7,9,11,9,10]
+
+
+
+ax.scatter(x, y, z, c='r', marker='o')
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
